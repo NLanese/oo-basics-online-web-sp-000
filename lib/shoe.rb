4b4328818a7eabd93d8bcb2efa1f9be8c98f04dd
Nick Lanese <nlanese21@gmail.com>
2020-07-24 03:25:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,22 @@
-# Make your shoe class here!
+class Shoe 
+  def initialize(brandInput)
+    @brand = brandInput
+  end
+  attr_reader :brand
+  attr_writer :brand
+  attr_reader :color
+  attr_writer :color
+  attr_reader :size
+  attr_writer :size
+  attr_reader :material
+  attr_writer :material
+  attr_reader :condition
+  attr_writer :condition
+  def cobble
+    @condition = "new"
+    puts "The shoe has been repaired!"
+  end
+end
+
+  
+  
