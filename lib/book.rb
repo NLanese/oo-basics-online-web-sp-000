827be8444684611c93f45059e8504f83ed0c3512
Nick Lanese <nlanese21@gmail.com>
2020-07-24 03:20:16 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,4 +3,17 @@ class Book
     @title = titleInput
   end
 
+  attr_accessor
+  attr_reader :title
+  attr_writer :title
+  attr_reader :author
+  attr_writer :author
+  attr_reader :genre
+  attr_writer :genre
+  attr_reader :page_count
+  attr_writer :page_count
+  def turn_page
+    @page_count += 1
+  end
+
 end
